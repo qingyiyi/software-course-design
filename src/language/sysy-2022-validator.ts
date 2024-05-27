d50ae8583cabf663f20aa45ea3eda88b3ccc7be1
qingyiyi <3097847063@qq.com>
2024-05-27 13:46:37 +0800

    实现了函数变量名重名报错

--- src/language/sysy-2022-validator.ts
+++ src/language/sysy-2022-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { Sysy2022AstType, Person } from './generated/ast.js';
+import { Sysy2022AstType, Model/*CompUnit*/, Person, CompUnit } from './generated/ast.js';
 import type { Sysy2022Services } from './sysy-2022-module.js';
 
 /**
@@ -9,7 +9,10 @@ export function registerValidationChecks(services: Sysy2022Services) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.Sysy2022Validator;
     const checks: ValidationChecks<Sysy2022AstType> = {
-        //Person: validator.checkPersonStartsWithCapital
+        //Person: validator.checkPersonStartsWithCapital,
+        Model: validator.checkifunique,
+        CompUnit: validator.checkblockitemunique
+        //MulExp_temp: validator.checkifzeroasdividend
     };
     registry.register(checks, validator);
 }
@@ -27,5 +30,64 @@ export class Sysy2022Validator {
             }
         }
     }
+
+    checkifunique(m:Model, accept: ValidationAcceptor): void {
+        const reported = new Set();
+        m.compUnit.forEach(d =>
+        {
+            if(d.varname){
+                if (reported.has(d.varname)) 
+                {
+                    accept('error',  `Def has non-unique name '${d.varname}'.`,  {node: d, property: 'varname'});
+                }
+                reported.add(d.varname);
+            }
+            if(d.constvarname)
+            {
+                if (reported.has(d.constvarname)) 
+                {
+                    accept('error',  `Def has non-unique name '${d.constvarname}'.`,  {node: d, property: 'constvarname'});
+                }
+                reported.add(d.constvarname);
+            }
+            if(d.funcname){
+                if (reported.has(d.funcname)) 
+                {
+                    accept('error',  `Def has non-unique name '${d.funcname}'.`,  {node: d, property: 'funcname'});
+                }
+                reported.add(d.funcname);
+            }
+        })
+    }
+
+    checkblockitemunique(m:CompUnit, accept: ValidationAcceptor): void {
+        const reported = new Set();
+        m.blockItem.forEach(d =>
+        {
+            if(d.varname){
+                if (reported.has(d.varname)) 
+                {
+                    accept('error',  `Def has non-unique name '${d.varname}'.`,  {node: d, property: 'varname'});
+                }
+                reported.add(d.varname);
+            }
+            if(d.constvarname)
+            {
+                if (reported.has(d.constvarname)) 
+                {
+                    accept('error',  `Def has non-unique name '${d.constvarname}'.`,  {node: d, property: 'constvarname'});
+                }
+                reported.add(d.constvarname);
+            }
+        })
+    }
+
+    // checkifzeroasdividend(mulexp: MulExp_temp, accept: ValidationAcceptor): void {
+    //     if(mulexp.mulop == '/')
+    //     {
+    //         if(!mulexp.numbers)
+    //             accept('error', '0 cannot be dividend', {node: mulexp, property: 'numbers'});
+    //     }
+    // }
 
 }
