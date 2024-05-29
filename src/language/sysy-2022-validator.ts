e2d180e2d384e27cc7e9f8562314b16ed19f79ad
self-propell <1134772893@qq.com>
2024-05-29 16:27:19 +0800

    完成了调用函数的参数数量匹配，参数类型匹配 debug needed

--- src/language/sysy-2022-validator.ts
+++ src/language/sysy-2022-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import { Sysy2022AstType, Model/*CompUnit*/, Person, CompUnit } from './generated/ast.js';
+import { Sysy2022AstType, Model/*CompUnit*/,CompUnit,  UnaryExp} from './generated/ast.js';
 import type { Sysy2022Services } from './sysy-2022-module.js';
 
 /**
@@ -9,9 +9,9 @@ export function registerValidationChecks(services: Sysy2022Services) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.Sysy2022Validator;
     const checks: ValidationChecks<Sysy2022AstType> = {
-        //Person: validator.checkPersonStartsWithCapital,
-        Model: validator.checkifunique,
-        CompUnit: validator.checkblockitemunique
+        //Model: validator.checkifunique,
+        CompUnit: validator.setFuncNameAndParams,
+        UnaryExp: validator.checkFuncNameAndParams
         //MulExp_temp: validator.checkifzeroasdividend
     };
     registry.register(checks, validator);
@@ -20,16 +20,9 @@ export function registerValidationChecks(services: Sysy2022Services) {
 /**
  * Implementation of custom validations.
  */
-export class Sysy2022Validator {
+const canshuMap = new Map();
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+export class Sysy2022Validator {
 
     checkifunique(m:Model, accept: ValidationAcceptor): void {
         const reported = new Set();
@@ -89,5 +82,68 @@ export class Sysy2022Validator {
     //             accept('error', '0 cannot be dividend', {node: mulexp, property: 'numbers'});
     //     }
     // }
+    
+    // 此函数用于检测函数并将<函数名，函数参数列表>加入到map中
+    setFuncNameAndParams(unit:CompUnit, accept: ValidationAcceptor):void{
+        let params:string[]=unit.paramtype;
+        if(unit.funcname){
+            let funcname:string=unit.funcname;
+            // 函数名已经在函数列表中
+            if(canshuMap.has(unit.funcname)){
+                // 参数发生了编辑
+                if(canshuMap.get(funcname)!==params){
+                    canshuMap.delete(funcname);
+                    canshuMap.set(funcname,params);
+                }
+                accept('info', `debuginfo name:'${unit.funcname}' params:'${params}'`, {node: unit, property: 'funcname'});
+            }
+            // 函数名不在参数列表中，将名和参数列表加入到map中
+            else{
+                canshuMap.set(funcname,params);
+                accept('info', `debuginfo name:'${unit.funcname}' params:'${params}'`, {node: unit, property: 'funcname'});
+            }
+        }
+    }
 
+    // 此函数用于在调用函数时，检查函数是否已声明/参数列表是否正确
+    checkFuncNameAndParams(unit:UnaryExp, accept: ValidationAcceptor):void{
+        //let funcname = unit.unaryFuncname;
+        //accept('error', `debug info: 调用的函数名为'${funcname}'`, {node: unit, property: 'unaryFuncname'});
+        if(unit.unaryFuncname&&canshuMap.has(unit.unaryFuncname)){
+            let funcname:string = unit.unaryFuncname;
+            let params:string[]=canshuMap.get(funcname);
+            // 调用时有参数
+            if(unit.unaryFuncRParams){
+                // 函数无参数
+                if(params==undefined){
+                    accept('error', `不匹配的参数数量!该函数无需传参`, {node: unit, property: 'unaryFuncRParams'});
+                    return;
+                }
+                let diaoparamsNumber:number=unit.unaryFuncRParams.unaryFuncParamNums.length;
+                // 参数数量不匹配
+                if(diaoparamsNumber!==params.length){
+                    accept('error', `不匹配的参数数量! 需要 ${params.length} 个参数 debuginfo:current params:'${diaoparamsNumber}'`, {node: unit, property: 'unaryFuncRParams'});
+                }
+                // 匹配参数类型
+                // ******************** to do **********************
+                else{
+                    for (let i = 0; i < params.length; i++) {
+                        if(params[i]=='int'){
+                            if(unit.unaryFuncRParams.unaryFuncParamNums){
+                                accept('error', `this is a debug info:'${unit.unaryFuncRParams.unaryFuncParamNums[i].toLocaleString}'`, {node: unit, property: 'unaryFuncRParams'});
+                            }
+                        }
+                    }
+                }
+            }
+            // 调用时无参数
+            else{
+                if(params!==undefined&&params.length!==0)
+                    accept('error', `不匹配的参数数量! 需要 '${params.length}' 个参数 debuginfo:current params:0`, {node: unit, property: 'unaryFuncRParams'});
+                }
+            
+            
+
+        }     
+    }
 }
